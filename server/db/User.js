84114e956bcdd6f23906bc1542da86905a686874
mjgilfus <mjgilfus@hotmail.com>
2023-06-13 12:40:48 -0400

    almost done

--- server/db/User.js
+++ server/db/User.js
@@ -2,10 +2,98 @@ const Sequelize = require('sequelize');
 const db = require('./db');
 
 const User = db.define('user', {
-  // Add your Sequelize fields here
-
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  userType: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [['STUDENT', 'TEACHER']],
+    },
+    defaultValue: 'STUDENT',
+  },
+  isStudent: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      const studentValue = this.getDataValue('userType');
+      return studentValue === 'STUDENT' ? true : false;
+    },
+  },
+  isTeacher: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      const teacherValue = this.getDataValue('userType');
+      return teacherValue === 'TEACHER' ? true : false;
+    },
+  },
 });
 
+User.findUnassignedStudents = async function () {
+  return User.findAll({
+    where: {
+      userType: 'STUDENT',
+      mentorId: null,
+    },
+  });
+};
+
+User.findTeachersAndMentees = async function () {
+  return this.findAll({
+    where: {
+      userType: 'TEACHER',
+    },
+    include: {
+      model: User,
+      as: 'mentees',
+    },
+  });
+};
+
+User.deleteUser = async function (identifier) {
+  User.destroy({
+    where: {
+      id: identifier,
+    },
+  });
+};
+
+User.doesIdExist = async function (identifer) {
+  const findId = await User.findOne({ where: { id: identifer } });
+  console.log(findId);
+
+  if (findId != null) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+User.beforeUpdate(async (user) => {
+  const teacherId = user.mentorId;
+  const findId = await User.findOne({ where: { id: teacherId } });
+  if (teacherId !== null && findId.userType !== 'TEACHER') {
+    throw new Error('Mentor is not a teacher');
+  }
+});
+
+User.beforeUpdate(async (user) => {
+  if (user.userType === 'TEACHER' && user.mentorId) {
+    throw new Error('A teacher cannot have a mentor');
+  }
+});
+
+User.beforeUpdate(async (user) => {
+  const hasMentees = await User.findOne({ where: { mentorId: user.id } });
+  if (user.userType === 'STUDENT' && hasMentees) {
+    throw new Error('A student cannot have mentees');
+  }
+});
 
 /**
  * We've created the association for you!
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const {
   models: { User },
 } = require('../db');
-
 /**
  * All of the routes in this are mounted on /api/users
  * For instance:
@@ -15,7 +14,80 @@ const {
  * possible to pass the bulk of these tests after having properly configured
  * the User model's name and userType fields.
  */
-
 // Add your routes here:
-
+router.get('/unassigned', async (req, res, next) => {
+  try {
+    const users = await User.findUnassignedStudents();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get('/teachers', async (req, res, next) => {
+  try {
+    const teachers = await User.findTeachersAndMentees();
+    res.json(teachers);
+  } catch (error) {
+    next(error);
+  }
+});
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    if (isNaN(id)) {
+      const err = new Error();
+      err.status = 400;
+      throw err;
+    }
+  } catch (error) {
+    next(error);
+  }
+  try {
+    const id = req.params.id;
+    const doesIdExist = await User.findByPk(+id);
+    if (doesIdExist === null) {
+      const err = new Error();
+      err.status = 404;
+      throw err;
+    }
+    res.status(204);
+    await User.deleteUser(id);
+    res.send('User Deleted');
+  } catch (error) {
+    next(error);
+  }
+});
+router.post('/', async (req, res, next) => {
+  try {
+    if (await User.findOne({ where: { name: req.body.name } })) {
+      const err = new Error();
+      err.status = 409;
+      throw err;
+    }
+    res.status(201);
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+router.put('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const findId = await User.findByPk(+id);
+    if (findId === null) {
+      const err = new Error();
+      err.status = 404;
+      throw err;
+    }
+    res.status(200);
+    const update = await findId.update({
+      name: req.body.name,
+      userType: req.body.userType,
+    });
+    res.json(update);
+  } catch (error) {
+    next(error);
+  }
+});
 module.exports = router;
